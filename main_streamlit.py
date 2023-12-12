d401e7c687539021c5b9862cf5df38622b716afc
avcton <avcton@gmail.com>
2023-12-12 23:19:58 +0500

    streamlit integeration

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,43 +1,36 @@
 import numpy as np
 import pandas as pd
-from taipy.gui import Gui
-
-
-class Course():
-    def __init__(self, code: str, title: str) -> None:
-        self.Code = code
-        self.Title = title
-        self.View = code + ' - ' + title
+import streamlit as st
 
 
 class DateSheet():
-    def __init__(self, path: str = None, title: str = None, semester: str = None,) -> None:
+    def __init__(self, file: any = None, title: str = None, semester: str = None,) -> None:
         self.title = title
         self.semester = semester
-        self.excel_file_path = path
+        self.file = file
         self.loaded = False
         self.dateSheet_columns = ['Date', 'Day', 'Time', 'Code', 'Course']
         self.dateSheet = pd.DataFrame(columns=self.dateSheet_columns)
         self.courses = []
 
-        if self.excel_file_path != None:
-            self.load(self.excel_file_path)
+        if self.file != None:
+            self.load(self.file)
             self.loaded = True
 
-    def load(self, path: str) -> None:
+    def load(self, file: any) -> None:
 
-        if self.excel_file_path == None:
-            self.excel_file_path = path
+        if self.file == None:
+            self.file = file
 
         if self.title == None:
             self.title = pd.read_excel(
-                self.excel_file_path, header=None, nrows=1).iloc[0, 0]
+                self.file, header=None, nrows=1).iloc[0, 0]
 
         if self.semester == None:
-            self.semester = pd.read_excel(self.excel_file_path, header=None,
+            self.semester = pd.read_excel(self.file, header=None,
                                           skiprows=1, nrows=1).iloc[0, 0]
 
-        dateSheet = pd.read_excel(self.excel_file_path, skiprows=2)
+        dateSheet = pd.read_excel(self.file, skiprows=2)
 
         dateSheet_refined = dateSheet.melt(id_vars=['Day', 'Date', 'Code'], value_vars=[
                                            dateSheet.columns[3]], var_name='Time', value_name='Course')
@@ -69,12 +62,84 @@ class DateSheet():
         self.dateSheet = dateSheet_refined
 
         for indx, row in self.dateSheet[['Code', 'Course']].iterrows():
-            self.courses.append(Course(row.Code, row.Course))
+            self.courses.append({
+                'Code': row.Code,
+                'Title': row.Course,
+                'View': row.Code + ' - ' + row.Course
+            })
 
-    def readByCode(self, codes: list):
+        self.loaded = True
+
+    def readByCode(self, codes: list[str]):
         mask = np.zeros(self.dateSheet.shape[0], dtype=bool)
 
         for code in codes:
             mask = mask | (self.dateSheet['Code'] == code)
 
         return self.dateSheet[mask]
+
+    def readByCourse(self, courses: list[dict]):
+        mask = np.zeros(self.dateSheet.shape[0], dtype=bool)
+
+        for course in courses:
+            mask = mask | (self.dateSheet['Code'] == course['Code'])
+
+        return self.dateSheet[mask]
+
+
+@st.cache_data
+def instantiate(file: any):
+    return DateSheet(file)
+
+
+# Global DateSheet Currently Loaded
+
+dateSheet = DateSheet()
+result = None
+
+# Initializing Page Settings
+
+st.set_page_config(
+    page_title="Datesheet Analyzer",
+    page_icon="icons/favicon.svg",
+    menu_items={
+        "Get Help": None,
+        'Report a bug': 'mailto:avcton@gmail.com',
+        'About': "This app is built to serve as a utility tool. Cheers ;)"
+    },
+    layout="wide")
+
+st.title(":rainbow[FAST] Datesheet Analyzer")
+
+# Fetching File from User
+
+uploaded_file = st.file_uploader(
+    "Upload the Latest Excel File", type=['xls', 'xlsx', 'xlsm'])
+
+if uploaded_file is not None:
+    dateSheet = instantiate(uploaded_file)
+    result = dateSheet.dateSheet
+
+# Displaying the Interaction UI + DateSheet Uploaded
+
+filters_col, showcase_col = st.columns(2)
+
+with filters_col:
+
+    filters = st.multiselect(
+        "Enter your Subjects", dateSheet.courses,
+        format_func=lambda course: course['View'],
+        key="filters")
+
+    if len(filters) > 0:
+        result = dateSheet.readByCourse(filters)
+
+
+with showcase_col:
+    if result is not None:
+        text_col1, text_col2 = st.columns(2)
+        text_col1.caption(dateSheet.title)
+        text_col2.caption(dateSheet.semester)
+        st.dataframe(result, hide_index=True)
+
+st.divider()
